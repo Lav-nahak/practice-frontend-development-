66781f3d3714e7d1d461c43c35402720186f62aa
Lav-nahak <146337272+Lav-nahak@users.noreply.github.com>
2024-10-13 22:33:13 +0000

    new

---
+++ basic/data_type.js
@@ -0,0 +1,50 @@
+let  score= 33
+console.log(score);
+console.log(typeof score)
+
+///string to number /////............
+  let score2="33"
+  console.log( typeof score2)
+ 
+// conversion to number
+ let valueInNumber=Number (score2)
+console.log(typeof valueInNumber)
+
+
+let score3="123ab"
+console.log(typeof score3)
+
+
+let valueInNumber3=Number (score3)
+console.log(typeof valueInNumber3)
+
+
+// null to number......................
+
+let score4=null
+console.log(typeof score4)
+
+
+// conversion to number 
+let ValueInNumber4=Number (score4)
+console.log(typeof ValueInNumber4)
+console.log(ValueInNumber4)
+
+
+let score5= undefined
+console.log(score5)
+console.log(typeof score5)
+let valueInNumber5=Number (score5)
+console.log(typeof valueInNumber5)
+console.log(valueInNumber5)
+
+
+
+// conversion  boolean top number
+  let score6= true
+  console.log(score6)
+console.log(typeof score6)
+let valueInNumber6=Number (score6)
+console.log(typeof valueInNumber6)
+console.log(valueInNumber6)
+
