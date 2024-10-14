75f0b639d454baed6c21ef1ec726e499ee6f0411
Lav-nahak <146337272+Lav-nahak@users.noreply.github.com>
2024-10-14 16:57:18 +0000

    todays practice

---
+++ po_2.js
@@ -0,0 +1,29 @@
+let score6= true
+console.log(score6)
+console.log(typeof score6)
+let valueInNumber6=Number (score6)
+console.log(typeof valueInNumber6)
+console.log(valueInNumber6)
+
+
+// let num=3
+// console.log(num);
+//  let name = "sangram"
+//  console.log( name);
+  
+
+
+var str= " this is my paradigm"
+let position = str.indexOf("is")
+console.log( position);
+ 
+let position3 = str.lastIndexOf("is")
+console.log(position3 );
+
+
+let position4=str.slice(1,3)
+console.log(position4);
+   let replace =str.replace('my', 'our')
+
+   console.log(replace);
+   
